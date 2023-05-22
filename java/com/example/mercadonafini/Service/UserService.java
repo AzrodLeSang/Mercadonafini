fa7834b9161b1e296c5e2d911b8a201fd047cd7c
AzrodLeSang <122784144+AzrodLeSang@users.noreply.github.com>
2023-05-23 00:29:06 +0200

    Delete java/com/example/mercadonafini directory

--- java/com/example/mercadonafini/Service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.example.mercadonafini.Service;
-
-import com.example.mercadonafini.model.UserModel;
-
-public interface UserService {
-
-    UserModel getUserByUsername (String username);
-}
