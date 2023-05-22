fd27f8b5264c275903837216cc51d2c073e20449
AzrodLeSang <122784144+AzrodLeSang@users.noreply.github.com>
2023-05-22 14:48:36 +0200

    Add files via upload

---
+++ test/java/com/example/mercadonafini/MercadonafiniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mercadonafini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadonafiniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
