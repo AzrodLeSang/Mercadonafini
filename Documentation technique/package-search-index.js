9409291b04b0f3d90e69199671aa405da7d49b99
AzrodLeSang <122784144+AzrodLeSang@users.noreply.github.com>
2023-05-23 11:06:04 +0200

    Delete Documentation technique directory

--- Documentation technique/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.example.mercadonafini"},{"l":"com.example.mercadonafini.Controller"},{"l":"com.example.mercadonafini.model"},{"l":"com.example.mercadonafini.repository"},{"l":"com.example.mercadonafini.Service"},{"l":"com.example.mercadonafini.Service.impl"}];updateSearchResults();
